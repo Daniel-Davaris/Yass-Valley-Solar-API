9fdb4a253ee64c04480d00a632eaf775e2d292de
Stephan-Kashkarov <kashkarovWork@gmail.com>
2020-02-22 14:43:26 +1100

    fixed the login route for real this time

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app, db
-from flask import request, render_template, flash, redirect, url_for, url_parse
+from app.models import User
+from flask import request, render_template, flash, redirect, url_for
 from flask_login import current_user, logout_user, login_user
 from app.forms import LoginForm
 
@@ -27,7 +28,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page:
             next_page = url_for('dashboard')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
@@ -37,17 +38,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('index'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(username=form.username.data, email=form.email.data)
-#         user.set_password(form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Congratulations, you are now a registered user!')
-#         return redirect(url_for('login'))
-#     return render_template('register.html', title='Register', form=form)
