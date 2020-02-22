671187412e6b98dedf9bba6627b9675d367f633a
daniel davaris <danieldavarispriv@gmail.com>
2020-02-22 14:39:57 +1100

    forms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import request, render_template, flash, redirect, url_for
 from flask_login import current_user, logout_user
-from app.forms import LoginForm
+from forms import LoginForm
 
 @app.route('/', methods=["GET", 'POST'])
 def dashboard():
